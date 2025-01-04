fb823cb94b6d9e5abfb1a606009f8347683ce0bc
Gregor Makowski <gregor.j.makowski@gmail.com>
2025-01-04 12:32:09 +0100

    add select all button and style the page

--- scr/components/data_dropdown.py
+++ scr/components/data_dropdown.py
@@ -8,6 +8,10 @@ def render(app:Dash)->html.Div:
     #TODO: Data have to be changed to the actual data
     all_data = ["country1","country2","country3"]
 
+    @app.callback(Output(ids.DATA_DROPDOWN,"value"),Input(ids.SELECT_ALL_BUTTON,"n_clicks"))
+    def select_all_countries(n_clicks:int)->list[str]:
+        return all_data
+
     return html.Div(
         children=[
             html.H6("Select a country"),
@@ -17,6 +21,11 @@ def render(app:Dash)->html.Div:
                 options=[{"label":data,"value":data} for data in all_data],
                 value=all_data,
                 multi=True                
+            ),
+            html.Button(
+                className=ids.DROPDOWN_BUTTON,
+                id=ids.SELECT_ALL_BUTTON,
+                children=["Select all countries"]
             )
         ]
     )
--- scr/components/ids.py
+++ scr/components/ids.py
@@ -5,3 +5,5 @@ HEADER1_NAME = "header1"
 
 DATA_DROPDOWN = "data-dropdown"
 DROPDOWN_CONTAINER = "dropdown-container"
+DROPDOWN_BUTTON = "dropdown-button"
+SELECT_ALL_BUTTON = "select-all-button"
