bc612f3810227488a67095dd187c578dd43ab97a
Gregor Makowski <gregor.j.makowski@gmail.com>
2025-01-04 12:01:44 +0100

    initialize app

--- scr/main.py
+++ scr/main.py
@@ -3,7 +3,7 @@ from dash_bootstrap_components.themes import BOOTSTRAP
 
 from components.layout import create_layout
 
-def main():
+def main()->None:
     #intitalize the app
     app = Dash(external_stylesheets=[BOOTSTRAP])
     #set title
@@ -11,7 +11,7 @@ def main():
     #create layout
     app.layout = create_layout(app)
     #run the app
-    app.run_server(debug=True)
+    app.run(debug=True)
 
 
 if __name__ == "__main__":
