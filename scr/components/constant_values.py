586e9cd9107a87317551aedd6abc89edba0cd251
Gregor Makowski <gregor.j.makowski@gmail.com>
2025-01-07 18:50:24 +0100

    delete all from media and add preview.png file

--- scr/components/constant_values.py
+++
@@ -1,9 +0,0 @@
-"""Constant values of the dashboard"""
-
-
-START_YEAR:int = 1990
-
-DATA_URL:str="https://nyc3.digitaloceanspaces.com/owid-public/data/co2/owid-co2-data.csv"
-
-DATA_SOURCE:str="Data source: Our world in data: "
-DATA_SOURCE_URL:str="https://github.com/owid/co2-data"
