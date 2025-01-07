0fc3ad9125202294d5f12c5ca4bbe8ddcb806fcf
Gregor Makowski <gregor.j.makowski@gmail.com>
2025-01-07 12:50:42 +0100

    refactor data_dropdown to dropdown_country

--- scr/components/layout.py
+++ scr/components/layout.py
@@ -1,5 +1,5 @@
 from dash import Dash, html
-from .data_dropdown import render as data_dropdown
+from .dropdown_country import render as dropdown_country
 from .choropleth import render as choropleth
 import components.ids as ids
 
@@ -10,7 +10,7 @@ def create_layout(app:Dash)->html.Div:
                         html.H1(id=ids.HEADER1_NAME,children="Dashboard to show geospatial data"),
                         html.Hr(),
                         html.Div(className=ids.DROPDOWN_CONTAINER,
-                                 children=[data_dropdown(app)]),
+                                 children=[dropdown_country(app)]),
                         html.Div(className=ids.CHOROPLETH_CONTAINER,
                                  children=[choropleth(app)])
                     ]
