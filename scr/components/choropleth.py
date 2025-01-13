0031a263892f9168fd37991feeaf256332f8366f
Gregor Makowski <gregor.j.makowski@gmail.com>
2025-01-13 15:24:24 +0100

    make countries selectable with co2 cumulative and bugfix

--- scr/components/choropleth.py
+++ scr/components/choropleth.py
@@ -67,8 +67,6 @@ def render(app:Dash, values:Values)->html.Div:
     fig.add_trace(
         render_trace(df=df_unselected,trace_name=ids.UNSELECTED_COUNTRIES, opacity=0.4).data[0]
         )
-    #update the trace
-    fig.update_traces(render_trace_update())
 
     #update the layout
     fig.update_layout(
@@ -78,15 +76,17 @@ def render(app:Dash, values:Values)->html.Div:
     #update the font of the colorbar
     fig.update_coloraxes(render_colorbar_update())
 
-    #update the region
-    fig.update_geos(
-        render_geo_update(values.region)
-        )
-
-
     #add datasource annotation
     fig.add_annotation(render_annotation(values))
 
+    fig.update_traces(
+        render_trace_update()
+    )
+
+    #update the region
+    fig.update_geos(
+            render_geo_update(values.region)
+    )
 
     @app.callback(Output(ids.CHOROPLETH_GRAPH, "figure"),
                 [Input(ids.DROPDOWN_CONTINENT, "value"),
@@ -98,7 +98,7 @@ def render(app:Dash, values:Values)->html.Div:
                 )
     def update_choropleth(region:str, year:int, clickData_choropleth:dict, clickData_suplots:dict, figure:dict)->dict:
         
-        if ctx.triggered_id == ids.CHOROPLETH_GRAPH:
+        if not ctx.triggered_id == ids.SUPLOTS_GRAPH:
             #set selected countries:
             if (not region == values.region) and (len(values.country_iso_codes)) > 0:
                 pass
@@ -112,39 +112,61 @@ def render(app:Dash, values:Values)->html.Div:
                     clickData_choropleth = {"points":[{'location':values.DEFAULT_NATION[region]}]}
                 if clickData_choropleth is not None:
                     values.select_country(iso_code=clickData_choropleth["points"][0]['location'])
-        elif ctx.triggered_id == ids.SUPLOTS_GRAPH:
+
+        else:
             if clickData_suplots is not None and clickData_suplots["points"][0]["curveNumber"]%2 == 1:
                 values.select_one_country(iso_code=figure["data"][clickData_suplots["points"][0]["curveNumber"]]["name"][0:3])
+
 
         df_selected, df_unselected = get_selected_unselected_data(df, year, values.country_iso_codes)
 
         #create the choropleth map
 
-        fig.update_traces(
+        trance = render_trace(df=df_selected, trace_name=ids.SELECTED_COUNTRIES)
+        trance.add_trace(
+            render_trace(df=df_unselected,trace_name=ids.UNSELECTED_COUNTRIES, opacity=0.4).data[0]
+            )
+
+        trance.update_traces(
             locations=df_selected["iso_code"],
             z=df_selected["co2_per_capita"],
             customdata=df_selected["country"],
             selector={"name":ids.SELECTED_COUNTRIES}
         )
-        fig.update_traces(
+        trance.update_traces(
             locations=df_unselected["iso_code"],
             z=df_unselected["co2_per_capita"],
             customdata=df_unselected["country"],
             selector={"name":ids.UNSELECTED_COUNTRIES}
             )
         
-        fig.update_geos(
+        #update the layout
+        trance.update_layout(
+        render_layout(year, values)
+        )
+
+        #update the font of the colorbar
+        trance.update_coloraxes(render_colorbar_update())
+
+        #add datasource annotation
+        trance.add_annotation(render_annotation(values))
+
+        
+        #update the trace
+        trance.update_traces(render_trace_update())
+        
+        trance.update_geos(
             scope=region
         )
 
-        fig.update_layout(
+        trance.update_layout(
             uirevision = region
         )
 
         values.region = region
         values.year = year
 
-        return {"data": fig.data, "layout": fig.layout}
+        return {"data": trance.data, "layout": trance.layout}
         
 
     return html.Div(
